bfbce8175c2068f8553a2b85cfedb84500e9d134
Jeff Billimek <jeffrey_k_billimek@homedepot.com>
2020-05-15 23:43:02 -0400

    arm64 bootstrapping

---
+++ k3os/arm64/bootstrap-k3os.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+message() {
+  echo -e "\n######################################################################"
+  echo "# $1"
+  echo "######################################################################"
+}
+
+if [ $# -eq 0 ]
+  then
+    echo "No node name supplied: call the script with '$0 <some node name> (optional <some target path>)'"
+    exit 1
+else
+  NODE="$1"
+fi
+
+if [ -z "$2" ]
+  then
+    # check if the ubuntu system-boot volume is already present in the default location
+    if [ -f /Volumes/system-boot/cmdline.txt ]
+      then
+        echo "Assuming /Volumes/system-boot as the target"
+        TARGET_VOLUME="/Volumes/system-boot"
+      else
+        echo "No path given as a target. Call the script with '$0 <some node name> <some target path>'"
+        exit 1
+    fi
+else
+  TARGET_VOLUME="$2"
+fi
+
+export REPO_ROOT=$(git rev-parse --show-toplevel)
+. "$REPO_ROOT"/.env
+
+message "writing $NODE configuration to $TARGET_VOLUME"
+
+echo "copying cmdline.txt to $TARGET_VOLUME/cmdline.txt"
+cp -f cmdline.txt "$TARGET_VOLUME/cmdline.txt"
+echo "copying user-data-${NODE} to $TARGET_VOLUME/user-data"
+envsubst < "user-data-${NODE}" > "$TARGET_VOLUME/user-data"
